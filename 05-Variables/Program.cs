6133e7ac4ac0ec1407895f68f60df17265783715
Kauan <kauan.manzatoo@gmail.com>
2025-07-05 20:34:42 -0300

    positional arguments, interpolated strings, format strings, number formatting

--- 05-Variables/Program.cs
+++ 05-Variables/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml;
+using System.Globalization;
 
 Action printLine = () =>
     Console.WriteLine("\n----------------------------------------------------------\n");
@@ -70,4 +71,64 @@ int number = 13;
 Console.WriteLine($"number set to: {number}");
 number = default;
 Console.WriteLine($"number reset to its default: {number}");
+printLine();
+#endregion
+
+#region positional arguments
+// set current culture to US English so that all readers
+// see the same output as shown in the book
+CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+Console.WriteLine(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+string formattedPrice = string.Format(
+    format: "{0} apples cost {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
+
+// three parameter values can use named arguments
+Console.WriteLine("{0} {1} lived in {2}.",
+    arg0: "Kauan", arg1: "Manzato", arg2: "São Paulo");
+
+// four or more parameter values cannot use named arguments
+Console.WriteLine(
+    "{0} {1} lived in {2} and worked in the {3} team at {4}.",
+    "Kauan", "Manzato", "São Paulo", "Development", "Manzato Tech");
+printLine();
+#endregion
+
+#region interpolated strings
+// the following statement must be all on one line when using C# 10
+// or earlier. If using C# 11 or later, we can include a line break
+// in the middle of an expression but not in the string text
+Console.WriteLine($"{numberOfApples} apples cost {pricePerApple * numberOfApples:C}.");
+printLine();
+#endregion
+
+#region understanding format strings
+string applesText = "Apples";
+int applesCount = 1234;
+
+string bananasText = "Bananas";
+int bananasCount = 56789;
+
+Console.WriteLine(format: "{0,-10} {1,6}",
+    arg0: "Name", arg1: "Count");
+Console.WriteLine(format: "{0,-10} {1,6:N0}",
+    arg0: applesText, arg1: applesCount);
+Console.WriteLine(format: "{0,-10} {1,6:N0}",
+    arg0: bananasText, arg1: bananasCount);
+printLine();
+#endregion
+
+#region custom number formatting
+decimal value = 0.325M;
+Console.WriteLine("Currency: {0:C}, Percentage: {0:0.0%}", value);
+printLine();
 #endregion
