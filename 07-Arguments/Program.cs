7012aaf657e3f203c10acbbca5aaec25fdc4c873
Kauan <kauan.manzatoo@gmail.com>
2025-07-06 15:02:25 -0300

    new project for arguments section

---
+++ 07-Arguments/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.Versioning;
+
+WriteLine($"There are {args.Length} arguments.");
+
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
+
+if (args.Length < 3)
+{
+    WriteLine("You must specify two colors and cursor size, e.g.");
+    WriteLine("dotnet run red yellow 50");
+    return; // stop running
+}
+
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[0], ignoreCase: true);
+
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType: typeof(ConsoleColor),
+    value: args[1], ignoreCase: true);
+
+if (OperatingSystem.IsWindows())
+{
+    CursorSize = int.Parse(args[2]); // only supported on Windows
+}
+else
+{
+    WriteLine("Setting the cursor size is only supported on Windows.");
+}
