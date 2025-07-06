5864585e60de7695f3b2cbe72a2dd1e610ccd3a2
Kauan <kauan.manzatoo@gmail.com>
2025-07-05 22:16:35 -0300

    remove unused project for user input

--- 06-InputFromUser/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.Write("Type your first name and press ENTER: ");
-string? firstName = Console.ReadLine();
-
-Console.Write("Type your age and press ENTER: ");
-string age = Console.ReadLine()!;
-
-Console.WriteLine($"Hello, {firstName}, you look good for {age}.");
